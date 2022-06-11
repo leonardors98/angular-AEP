13be564a6a4168af852e2b9ba8764b268af2b7b2
leonardors98 <leonardo.rs.2105@gmail.com>
2022-06-11 16:58:02 -0300

    2 files modified, 1 deleted

--- src/app/view/login/login.component.ts
+++ src/app/view/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  buttonLoading: boolean;
   logoIMG: string =
     'https://github.com/leonardors98/angular-AEP/blob/master/src/app/assets/img/logoHC.png?raw=true';
 
