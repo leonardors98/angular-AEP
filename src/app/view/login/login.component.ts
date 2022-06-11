444f4768d7db9a680a2ffdba9d304588388a7e18
leonardors98 <leonardo.rs.2105@gmail.com>
2022-06-11 16:24:05 -0300

    2 files modified

--- src/app/view/login/login.component.ts
+++ src/app/view/login/login.component.ts
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  logoIMG: string =
+    'https://github.com/leonardors98/angular-AEP/blob/master/src/app/assets/img/logoHC.png?raw=true';
+
   constructor() {}
   ngOnInit() {}
 }
